428df819d03cf5fccc0540115ace125181b20619
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-03-12 17:54:16 +0000

    Github icon linked

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,10 +1,29 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
+
+const links = [
+	{ url: "/", title: "Home" },
+	{ url: "/about", title: "About" },
+	{ url: "/portfolio", title: "Portfolio" },
+	{ url: "/contact", title: "Contact" },
+];
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
 	return (
 		<div className="h-full items flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+			<div className="hidden md:flex gap-4">
+				{links.map((link) => (
+					<Link href={link.url} key={link.title}>
+						{link.title}
+					</Link>
+				))}
+			</div>
+
 			{/* LOGO */}
-			<div className="">
+			<div className="md:hidden">
 				<Link
 					href="/"
 					className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
@@ -14,17 +33,34 @@ const Navbar = () => {
 					</span>
 				</Link>
 			</div>
-			{/* NAVIGATION MENU */}
+
 			<div className="">
+				<Link href={"https://github.com/AlnafisVSCode"}>
+					<Image src="/github.png" alt="" width={24} height={24} />
+				</Link>
+			</div>
+
+			{/* NAVIGATION MENU */}
+			<div className="md:hidden">
 				{/* Menu Button */}
-				<button className="w-10 h-8 flex flex-col justify-between">
+				<button
+					className="w-10 h-8 flex flex-col justify-between z-50 relative"
+					onClick={() => setOpen((prev) => !prev)}>
 					<div className="w-10 h-1 bg-black rounded"></div>
 					<div className="w-10 h-1 bg-black rounded"></div>
 					<div className="w-10 h-1 bg-black rounded"></div>
 				</button>
 
 				{/* MENU LIST */}
-				<div className=""></div>
+				{open && (
+					<div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+						{links.map((link) => (
+							<Link href={link.url} key={link.title}>
+								{link.title}
+							</Link>
+						))}
+					</div>
+				)}
 			</div>
 		</div>
 	);
