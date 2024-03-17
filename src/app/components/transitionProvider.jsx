555188cf1b5b3a35a25e648dbaacde9e67c4ca46
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-03-17 02:57:57 +0000

    smaller font

--- src/app/components/transitionProvider.jsx
+++ src/app/components/transitionProvider.jsx
@@ -19,7 +19,7 @@ const TransitionProvider = ({ children }) => {
 					transition={{ duration: 0.8, ease: "easeOut" }}
 				/>
 				<motion.div
-					className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
+					className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-7xl cursor-default z-50 w-fit h-fit"
 					initial={{ opacity: 1 }}
 					animate={{ opacity: 0 }}
 					exit={{ opacity: 0 }}
