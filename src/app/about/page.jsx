e494cf09b9f7d0325dd8adfd422280a1d6d83bb6
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-03-19 17:03:03 +0000

    Skills Listed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,11 +16,15 @@ const AboutPage = () => {
 					{/* Biography Container */}
 					<div className="flex flex-col gap-12 justify-center">
 						{/* Biography Title */}
-						<h1 className="font-bold text- 2xl:">Biograph</h1>
+						<h1 className="font-bold text-2xl">Biograph</h1>
 						{/* Biography Description */}
 						<p className="text-lg">
-							Kingdom. I specialize in building web applications and mobile apps
-							using modern technologies.
+							Hi, I am a passionate and dedicated software engineer with a
+							strong background in web development and a keen interest in the
+							latest technologies. I am a quick learner and a team player with
+							excellent communication skills. I am always eager to learn and
+							improve my skills and I am looking for a challenging role in a
+							reputable company where I can grow and develop my career.
 						</p>
 						{/* Biography Quote */}
 						<span className="italic">If you aint first You are last!</span>
@@ -43,9 +47,103 @@ const AboutPage = () => {
 						</div>
 					</div>
 					{/* Skills Container */}
-					<div className="">Skills</div>
+					<div className="flex flex-col gap-12 justify-center">
+						{/* Skills Title */}
+						<h1 className="font-bold text-2xl">Skills</h1>
+						{/* Skills List */}
+						<div
+							className="flex gap-4 flex-wrap justify-center
+						">
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								JavaScript
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								TypeScript
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								React.js
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Next.js
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								SCSS
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Tailwind CSS
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								MongoDB
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								PostgreSQL
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Node.js
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Nest.js
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Express.js
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Spring Boot
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								GraphQL
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Apollo
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Redux
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Framer Motion
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Vite
+							</div>
+
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								AWS
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Firebase
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Git
+							</div>
+							<div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+								Figma
+							</div>
+						</div>
+
+						{/* SKILL SCROLL SVG */}
+						<svg
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							width={50}
+							height={50}>
+							<path
+								d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+								stroke="#000000"
+								strokeWidth="1"></path>
+							<path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+							<path
+								d="M15 11L12 14L9 11"
+								stroke="#000000"
+								strokeWidth="1"></path>
+						</svg>
+					</div>
+
 					{/* Experience Container */}
-					<div className="">Experience</div>
+					<div className="flex flex-col gap-12 justify-center pb-52">
+						{/* Experience Title */}
+						<h1 className="font-bold text-2xl">Experience</h1>
+					</div>
 				</div>
 
 				{/* Svg Container */}
